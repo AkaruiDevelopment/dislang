c22fe53b81eea4252c25478c796b50ec2ce10b1f
JeffTheModder <jeffreyjr.morris@gmail.com>
2022-06-26 23:47:12 -0500

    Set up basic parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import {Literal, Node} from "./ast/ast";
-import {ast} from "./ast/test-ast";
-import {Transpiler} from "./transpiler/transpiler";
+// import {ast} from "./ast/test-ast";
+// import {Transpiler} from "./transpiler/transpiler";
+
+// try {
+//     let transplr: Transpiler = new Transpiler(ast);
+//     console.log(transplr.js);
+// } catch (e) {
+//     console.error(e);
+// }
+
+import {parser} from "./parser/parser";
 
 try {
-    let transplr: Transpiler = new Transpiler(ast);
-    console.log(transplr.js);
+    let parsed: Node = parser.parse(`var hi = "hey there"`);
+    console.log(JSON.stringify(parsed, null, 4));
 } catch (e) {
     console.error(e);
 }
